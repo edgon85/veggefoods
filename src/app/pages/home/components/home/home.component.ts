6a1b2ff947d5b6b7fc58827a86dec666cb2feee4
edgon <edgon85@gmail.com>
2020-03-18 12:55:43 -0600

    seccion 2 iconos de home

--- src/app/pages/home/components/home/home.component.ts
+++ src/app/pages/home/components/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
-// import Swiper from 'swiper';
 
 @Component({
   selector: 'app-home',
@@ -7,29 +6,7 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  // AfterViewInit
-  // mySwiper: Swiper;
-
   constructor() {}
 
   ngOnInit() {}
-
-  // ngAfterViewInit(): void {
-  //   this.mySwiper = new Swiper('.swiper-container', {
-  //     spaceBetween: 30,
-  //     centeredSlides: true,
-  //     autoplay: {
-  //       delay: 2500,
-  //       disableOnInteraction: false
-  //     },
-  //     pagination: {
-  //       el: '.swiper-pagination',
-  //       clickable: true
-  //     },
-  //     navigation: {
-  //       nextEl: '.swiper-button-next',
-  //       prevEl: '.swiper-button-prev'
-  //     }
-  //   });
-  // }
 }
