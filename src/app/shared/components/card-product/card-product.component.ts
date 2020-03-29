35b04c8e0b9e9f64858f6a3b9ed283eadcd6e529
edgon <edgon85@gmail.com>
2020-03-28 20:08:54 -0600

    funcionalidad boton agregara carrito en card

--- src/app/shared/components/card-product/card-product.component.ts
+++ src/app/shared/components/card-product/card-product.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ProductoModel } from '../../../models/product.model';
 import { Router } from '@angular/router';
+import { CartInterface } from '../../../interfaces/cart.interface';
+import { CartService } from '../../../services/cart.service';
+import Swal from 'sweetalert2';
+import { mergeAll, pluck } from 'rxjs/operators';
 
 @Component({
   selector: 'app-card-product',
@@ -10,12 +14,44 @@ import { Router } from '@angular/router';
 export class CardProductComponent implements OnInit {
   @Input() producto: ProductoModel;
 
-  constructor(private router: Router) {}
+  estaEnCarrito: boolean = false;
 
-  ngOnInit() {}
+  constructor(private router: Router, private cartService: CartService) {}
+
+  ngOnInit() {
+    this.revisarSiEstaEnCarrito();
+  }
 
   getProduct(productId: string, categoryPrd: string) {
     localStorage.setItem('redirectProd', `/productos/categoria/${categoryPrd}`);
     this.router.navigate(['/producto', productId]);
+  }
+
+  addToCart() {
+    const cart: CartInterface = {
+      cartItemId: this.producto._id,
+      quantity: 1,
+      product: this.producto
+    };
+
+    // console.log(cart);
+    this.cartService.addToCart(cart);
+    Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: `Se agrego ${this.producto.nombre} al carrito`,
+      showConfirmButton: false,
+      timer: 2500
+    });
+  }
+
+  revisarSiEstaEnCarrito() {
+    this.cartService.cart$
+      .pipe(mergeAll(), pluck('product', '_id'))
+      .subscribe(resp => {
+        if (resp === this.producto._id) {
+          this.estaEnCarrito = true;
+        }
+      });
   }
 }
