9f907b4d1756a47ce4cf59e489c0971f929b33be
edgon <edgon85@gmail.com>
2020-04-05 13:37:44 -0600

    instalando angularfire2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+
+// routing
 import { AppRoutingModule } from './app-routing.module';
+
+// componentes
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
 import { LayoutComponent } from './layout/layout.component';
+
+// modulos
+import { SharedModule } from './shared/shared.module';
+
+// angular fire2
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, LayoutComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule // imports firebase/auth, only needed for auth features,
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
