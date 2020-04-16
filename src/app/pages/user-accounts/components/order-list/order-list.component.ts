6dbe0d727b2ef85968a69f936dca1e7db3d81272
edgon <edgon85@gmail.com>
2020-04-15 22:01:15 -0600

    mis pedidos implementados

--- src/app/pages/user-accounts/components/order-list/order-list.component.ts
+++ src/app/pages/user-accounts/components/order-list/order-list.component.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../../services/user.service';
+import { Observable } from 'rxjs';
+import { OrderInterface } from '../../../../interfaces/order.interface';
+import { AuthService } from '../../../../services/auth.service';
 
 @Component({
   selector: 'app-order-list',
   templateUrl: './order-list.component.html',
-  styleUrls: ['./order-list.component.scss']
+  styleUrls: ['./order-list.component.scss'],
 })
 export class OrderListComponent implements OnInit {
+  //
 
-  constructor() { }
+  orders$: Observable<OrderInterface[]>;
+  cargando: boolean = false;
+  pedidoDetalle: boolean = false;
+
+  userUid: string = '';
+
+  order$: Observable<OrderInterface>;
+
+  constructor(
+    private authService: AuthService,
+    private userService: UserService
+  ) {}
 
   ngOnInit() {
+    this.cargando = true;
+    this.obtenerPedidos();
   }
 
+  /* <====================================================> */
+  /* <Obtener llistado de pedidos> */
+  /* <====================================================> */
+  obtenerPedidos() {
+    this.authService.getStatus().subscribe((resp) => {
+      this.userUid = resp.uid;
+      this.orders$ = this.userService.getAllOrders(resp.uid);
+      this.cargando = false;
+    });
+  }
+
+  /* <====================================================> */
+  /* Detalle de pedido */
+  /* <====================================================> */
+  verDetalle(orderId: string) {
+    console.log(this.userUid);
+    console.log(`orderId: ${orderId}`);
+    this.pedidoDetalle = true;
+
+    this.order$ = this.userService.getOrderItem(this.userUid, orderId);
+  }
+
+  /* <====================================================> */
+  /* Boton de regresar */
+  /* <====================================================> */
+  regresar() {
+    this.pedidoDetalle = false;
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,22 +2,42 @@ import { Injectable } from '@angular/core';
 import {
   AngularFirestore,
   AngularFirestoreDocument,
+  AngularFirestoreCollection,
 } from 'angularfire2/firestore';
 import { UsuarioModel } from '../interfaces/user.interface';
 import { Checkout } from '../interfaces/checkout.interface';
 import Swal from 'sweetalert2';
 import { Router } from '@angular/router';
 import { CartService } from './cart.service';
+import { Observable } from 'rxjs';
+import { OrderInterface } from '../interfaces/order.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
+  // /users/UxYEyuY2LLbXvsfOOXrr8wWHX5P2/order/1586759781169
+
+  private itemsCollection: AngularFirestoreCollection<OrderInterface>;
+  items: Observable<OrderInterface[]>;
+
+  private itemDoc: AngularFirestoreDocument<OrderInterface>;
+  item: Observable<OrderInterface>;
+
   constructor(
     private afs: AngularFirestore,
     private cartService: CartService,
     private router: Router
-  ) {}
+  ) {
+    /*     this.itemsCollection = afs
+      .collection<OrderInterface>('users')
+      .doc('UxYEyuY2LLbXvsfOOXrr8wWHX5P2')
+      .collection('order');
+    this.items = this.itemsCollection.valueChanges();
+
+    console.log(this.items);
+    console.log('que pedo que pedo'); */
+  }
 
   // ================================================================ //
   // Obtener usuario por uid //
@@ -49,5 +69,33 @@ export class UserService {
 
   updateUser(userUId: string, user: object) {
     return this.afs.doc(`users/${userUId}`).update(user);
+  }
+
+  // ================================================================ //
+  // Obtener pedidos //
+  // ================================================================ //
+  getAllOrders(userUid: string) {
+    this.itemsCollection = this.afs
+      .collection<OrderInterface>('users')
+      .doc(userUid)
+      .collection('order', (ref) => ref.orderBy('fechaCreacion', 'desc'));
+    this.items = this.itemsCollection.valueChanges();
+
+    return this.items;
+  }
+
+  // ================================================================ //
+  // Obtener un pedido //
+  // ================================================================ //
+
+  // /users/UxYEyuY2LLbXvsfOOXrr8wWHX5P2/order/1586759781169
+  getOrderItem(userUid: string, orderId: string) {
+    /*     this.itemDoc = this.afs.doc<OrderInterface>(
+      `users/${userUid}/order/${orderId}`
+    );
+    this.item = this.itemDoc.valueChanges(); */
+    return this.afs
+      .doc<OrderInterface>(`users/${userUid}/order/${orderId}`)
+      .valueChanges();
   }
 }
