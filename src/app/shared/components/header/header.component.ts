3dd7665e7323e9b00f07508583fa6b8e6511f820
Edwin Gonzalez <hgedwin@wedevgt.com>
2021-03-25 20:22:38 -0600

    implementando nuevo navbar

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { CartService } from '../../../services/cart.service';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { AuthService } from '../../../services/auth.service';
 import { Router } from '@angular/router';
+
+declare function init_plugin_navbar();
 
 @Component({
   selector: 'app-header',
@@ -18,7 +20,8 @@ export class HeaderComponent implements OnInit {
   constructor(
     private cartService: CartService,
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private elem: ElementRef
   ) {
     this.loggedIn();
     this.total$ = this.cartService.cart$.pipe(
@@ -26,7 +29,10 @@ export class HeaderComponent implements OnInit {
     );
   }
 
-  ngOnInit() {}
+
+  ngOnInit() {
+    init_plugin_navbar();
+  }
 
   logout() {
     this.authService
@@ -59,6 +65,28 @@ export class HeaderComponent implements OnInit {
       this.router.navigateByUrl('/busqueda/q=');
     } else {
       this.router.navigateByUrl('/cart');
+    }
+  }
+
+  removeClases() {
+    let menu = this.elem.nativeElement.querySelector('.menu-list');
+    let menubtn = this.elem.nativeElement.querySelector('.menu-btn');
+    let cartBtn = this.elem.nativeElement.querySelector('.cart-btn');
+    let searchBtn = this.elem.nativeElement.querySelector('.search-btn');
+    let myBody = this.elem.nativeElement.querySelector('body');
+    let dropdownTienda = this.elem.nativeElement.querySelector('.tienda');
+    let dropdownCuenta = this.elem.nativeElement.querySelector('.cuenta');
+
+
+
+    if (menu.classList.contains('active')) {
+      menu.classList.remove('active');
+      menubtn.classList.remove('hide');
+      cartBtn.classList.remove('hide');
+      searchBtn.classList.remove('hide');
+      // myBody.classList.remove('disabledScroll');
+      dropdownTienda.classList.remove('hide');
+      dropdownCuenta.classList.remove('hide');
     }
   }
 }
