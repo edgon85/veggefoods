3bd736796810b372d8fb4f1c62f17bd482ca2020
Edwin <edgon85@gmail.com>
2021-06-14 16:05:55 -0600

    fix FontAwesome in traking

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -5,9 +5,10 @@ import { SharedModule } from '../../shared/shared.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { BannerComponent } from './components/banner/banner.component';
 import { HomeCardsComponent } from './components/home-cards/home-cards.component';
+import { PromosComponent } from './components/promos/promos.component';
 
 @NgModule({
-  declarations: [HomeComponent, BannerComponent, HomeCardsComponent],
+  declarations: [HomeComponent, BannerComponent, HomeCardsComponent, PromosComponent],
   imports: [CommonModule, SharedModule, HomeRoutingModule]
 })
 export class HomeModule {}
